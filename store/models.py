6edc4804d98fac4fbc4b3e1c18aa72a5ec7c8727
MAULESH DEV <163156946+catxy07@users.noreply.github.com>
2025-12-20 11:27:41 +0530

    Update models.py

--- store/models.py
+++ store/models.py
@@ -1,58 +1,53 @@
-from tkinter import CASCADE
 from django.db import models
 from category.models import Category
 from django.urls import reverse
 from accounts.models import Account
-from django.db.models import Avg ,Count
+from django.db.models import Avg, Count
 
 # Create your models here.
 class Product(models.Model):
-    product_name = models.CharField(max_length=200,unique= True)
-    slug = models.SlugField(max_length=200,unique=True)
-    discription = models.TextField(max_length=500,blank=True)
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    discription = models.TextField(max_length=500, blank=True)
     price = models.IntegerField()
     images = models.ImageField(upload_to='photos/product')
     stock = models.IntegerField()
-    is_available =models.BooleanField(default=True)
-    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
     def get_url(self):
-        return reverse('product_detail',args=[self.category.slug, self.slug])
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
-    
     def __str__(self):
         return self.product_name
-    
+
     def averageReview(self):
-        reviews= ReviewRating.objects.filter(product=self,status=True).aggregate(average=Avg('rating'))
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
         avg = 0
         if reviews['average'] is not None:
             avg = float(reviews['average'])
         return avg
-    
+
     def countReview(self):
-        reviews= ReviewRating.objects.filter(product=self,status=True).aggregate(count=Count('id'))
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
         count = 0
         if reviews['count'] is not None:
             count = int(reviews['count'])
         return count
 
-    
+# Variations
 class VariationManager(models.Manager):
     def colors(self):
-        return super(VariationManager,self).filter(variation_category='color', is_active=True)
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
     
     def sizes(self):
-        return super(VariationManager,self).filter(variation_category='size', is_active=True)
-    
-    
-    
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
 
-variation_category_choice =(
-    ('color','color'),
-    ('size','size'),
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
 )
 
 class Variation(models.Model):
@@ -66,15 +61,15 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
-    
 
+# Reviews
 class ReviewRating(models.Model):
-    product =models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    subject = models.CharField(max_length=100,blank=True)
-    review = models.TextField(max_length=500,blank=True)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
     rating = models.FloatField()
-    ip = models.CharField(max_length=20,blank=True)
+    ip = models.CharField(max_length=20, blank=True)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -82,13 +77,14 @@ class ReviewRating(models.Model):
     def __str__(self):
         return self.subject
 
+# Product gallery
 class ProductGallery(models.Model):
-    product = models.ForeignKey(Product,default= None,on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='store/products/',max_length=255)
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/products/', max_length=255)
 
-    def __sizeof__(self):
+    def __str__(self):
         return self.product.product_name
     
     class Meta:
         verbose_name = 'productgallery'
-        verbose_name_plural ="product gallery"
+        verbose_name_plural = "product gallery"
