83cac3d44b4a7267d989765aaca328406596cd24
MAULESH DEV <163156946+catxy07@users.noreply.github.com>
2025-12-20 11:22:56 +0530

    Add superuser creation on post-migrate
    Automatically create a superuser on post-migrate if not exists.

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,23 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
+import os
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        if os.environ.get('RENDER'):  # Only run on Render
+            from django.contrib.auth import get_user_model
+
+            def create_superuser(sender, **kwargs):
+                User = get_user_model()
+                if not User.objects.filter(is_superuser=True).exists():
+                    User.objects.create_superuser(
+                        email="admin@admin.com",
+                        username="admin",
+                        password="admin123"
+                    )
+                    print("Superuser created: admin@admin.com / admin123")
+
+            post_migrate.connect(create_superuser, sender=self)
